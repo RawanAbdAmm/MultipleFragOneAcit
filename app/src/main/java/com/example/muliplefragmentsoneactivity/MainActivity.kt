1ab3cf5749c443f171c5b029ad28751eaca5835c
rawanabdamm <rawanabdamm@gmail.com>
2024-04-03 00:58:45 +0300

    add navigation and LoginFragment,ProductList and Details fragments

--- app/src/main/java/com/example/muliplefragmentsoneactivity/MainActivity.kt
+++ app/src/main/java/com/example/muliplefragmentsoneactivity/MainActivity.kt
@@ -1,37 +1,19 @@
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
-import com.example.muliplefragmentsoneactivity.R
 
+import androidx.appcompat.app.AppCompatActivity
+import com.example.muliplefragmentsoneactivity.R
+import androidx.navigation.fragment.NavHostFragment
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            replaceFragment(LoginFragment())
-        }
+        val navHostFragment = NavHostFragment.create(R.navigation.nav_graph)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, navHostFragment)
+            .setPrimaryNavigationFragment(navHostFragment)
+            .commit()
     }
-
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.commit {
-            replace( R.id.fragment_container, fragment)
-            addToBackStack(null)
-        }
-    }
-}
-
-class LoginFragment : Fragment() {
-        override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            return inflater.inflate(R.layout.fragment_login, container, false)
-        }
-
 }
