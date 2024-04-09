e11965986c9d690637f69b01a742e673f9524257
omaryousef <oyousef@ainnova.com>
2024-04-09 16:14:02 +0300

    Fix navigation problem.

--- app/src/main/java/com/example/muliplefragmentsoneactivity/MainActivity.kt
+++ app/src/main/java/com/example/muliplefragmentsoneactivity/MainActivity.kt
@@ -10,9 +10,6 @@ class MainActivity : AppCompatActivity() {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
 
-            supportFragmentManager.beginTransaction()
-                .add(R.id.nav_host_fragment, LoginFragment(), "LoginFragment")
-                .commit()
         }
 
 
